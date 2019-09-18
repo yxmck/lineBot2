91ceb9c95f7f7ad1e01a71eba5853190d8914f7c
yxmck <51776837+yxmck@users.noreply.github.com>
2019-09-18 17:41:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,10 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    user = decoded["events"][0]['replyToken']
-    userText = decoded["events"][0]['message']['text']
+    #user = decoded["events"][0]['replyToken']
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    #userText = decoded["events"][0]['message']['text']
+    userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
     if (userText == 'กินข้าวรึยัง') :
         sendText(user,'กินแล้ว')
